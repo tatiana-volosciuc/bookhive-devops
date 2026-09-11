97fc565581e397ed5b83afecd556ea908e042fe0
Tatiana Jarovlea <tatiana.jarovlea@amdaris.com>
2026-09-11 16:08:58 +0300

    feature/M9-rds-configuration: Added RDS configuration for terraform

--- terraform/network/iam.tf
+++ terraform/network/iam.tf
@@ -10,12 +10,32 @@ data "aws_iam_policy_document" "ec2_assume" {
 
 resource "aws_iam_role" "app_instance" {
   name               = "${var.project}-app-instance-role"
-  assume_role_policy = data.aws_iam_policy_document.ec2_assume.json
+  assume_role_policy = jsonencode({
+      Version = "2012-10-17"
+      Statement = [{
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = { Service = "ec2.amazonaws.com" }
+      }]
+    })
 }
 
 resource "aws_iam_role_policy_attachment" "ssm_core" {
   role       = aws_iam_role.app_instance.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_iam_role_policy" "app_secrets" {
+  name = "${var.project}-app-secrets-read"
+  role = aws_iam_role.app_instance.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect   = "Allow"
+      Action   = "secretsmanager:GetSecretValue"
+      Resource = aws_db_instance.main.master_user_secret[0].secret_arn
+    }]
+  })
 }
 
 resource "aws_iam_instance_profile" "app_instance" {
--- terraform/network/security_groups.tf
+++ terraform/network/security_groups.tf
@@ -13,3 +13,16 @@ resource "aws_security_group" "app" {
 
   tags = { Name = "${var.project}-app-sg" }
 }
+
+resource "aws_security_group" "db" {
+  name   = "${var.project}-db-sg"
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
+    security_groups = [aws_security_group.app.id]
+  }
+  tags = { Name = "${var.project}-db-sg" }
+}
